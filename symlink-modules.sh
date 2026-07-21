d63835af66604fb1e40205209a54426407a09893
SqrtMz <cvesgaduran@gmail.com>
2026-07-21 15:05:24 -0500

    added nvim to symlink

--- symlink-modules.sh
+++ symlink-modules.sh
@@ -5,3 +5,4 @@ ln -sf ${HOME}/mz-dotfiles/modules/flameshot ${HOME}/.config/
 ln -sf ${HOME}/mz-dotfiles/modules/alacritty ${HOME}/.config/
 ln -sf ${HOME}/mz-dotfiles/modules/kitty ${HOME}/.config/
 ln -sf ${HOME}/mz-dotfiles/modules/nix ${HOME}/.config/
+ln -sf ${HOME}/mz-dotfiles/modules/nvim ${HOME}/.config/
