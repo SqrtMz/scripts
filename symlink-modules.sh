fd8e595dcd1c1f7f6e7c822facae780e56b40dee
SqrtMz <cvesgaduran@gmail.com>
2026-07-21 12:39:05 -0500

    added nix to symlink

--- symlink-modules.sh
+++ symlink-modules.sh
@@ -1,6 +1,7 @@
 #! /usr/bin/env bash
 
-ln -sf ${HOME}/mz-dotfiles/modules/hypr ${HOME}/.config/hypr
-ln -sf ${HOME}/mz-dotfiles/modules/flameshot ${HOME}/.config/flameshot
-ln -sf ${HOME}/mz-dotfiles/modules/alacritty ${HOME}/.config/alacritty
-ln -sf ${HOME}/mz-dotfiles/modules/kitty ${HOME}/.config/kitty
+ln -sf ${HOME}/mz-dotfiles/modules/hypr ${HOME}/.config/
+ln -sf ${HOME}/mz-dotfiles/modules/flameshot ${HOME}/.config/
+ln -sf ${HOME}/mz-dotfiles/modules/alacritty ${HOME}/.config/
+ln -sf ${HOME}/mz-dotfiles/modules/kitty ${HOME}/.config/
+ln -sf ${HOME}/mz-dotfiles/modules/nix ${HOME}/.config/
